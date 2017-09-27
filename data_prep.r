989990b2b91b27e3afa37e06f92cfee4aa373daa
kambanite <kambanite@gmail.com>
2017-09-27 17:34:48 +0200

    quickly

--- data_prep.r
+++ data_prep.r
@@ -5,6 +5,7 @@ cat('\014')
 
 
 library(XLConnect)
+library(reshape2)
 
 
 # read in the data --------------------------------------------------------
@@ -45,6 +46,30 @@ for(i in 1:length(data.list2)){
 }
 lapply(data.list2, names)
 
+
+
+# caluclate differences ---------------------------------------------------
+
+calc_diff = function(x){
+  # x is a data frame
+  y = data.frame(x$p1 - x$inflow, x$p2 - x$inflow, x$p3 - x$inflow,
+                 x$p4 - x$inflow, x$p5 - x$inflow, x$p6 - x$inflow)
+  names(y) = paste0('dp', 1:6)
+  return(cbind.data.frame(x,y))
+}
+
+data.list3 = lapply(data.list2, calc_diff)
+
+# # reshape the data --------------------------------------------------------
+# 
+# pain = function(x){
+#   # x is the data frame
+#   y = melt(x, c('date'), c('inflow', paste0('p', 1:6)))
+#   names(y) = c('date', 'bf', lapply(x, unique)$nform)
+#   return(y)
+# } 
+# 
+# data.list3 = lapply(data.list2, pain)
 
 # data concatenation ------------------------------------------------------
 
